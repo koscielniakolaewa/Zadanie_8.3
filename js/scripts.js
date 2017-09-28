9cb9a034ce61e939a980d6f1d5096dc629c34bbb
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-09-28 15:07:28 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// pierwsza linijka message
+alert('Hello, ' + name);
+// druga linijka message
+console.log('Hello, ' + name);
+
+/*
+  block comment
+  console.log('The commented will not be called');
+  alert('Hello, ' + name);
+*/
